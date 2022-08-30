c28ca81f81100ec302aadac98ac42762f4ce9793
UZzoma <57295185+UZzoma@users.noreply.github.com>
2022-08-30 04:30:27 +0300

    Update util.ts

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -11,7 +11,14 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
+      const photo = await axios({
+        method: 'get',
+        url: inputURL,
+        responseType: 'arraybuffer'
+      })
+      .then(function ({data: imageBuffer}){
+        return Jimp.read(imageBuffer)
+      });
       const outpath =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
